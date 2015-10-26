b26c3b0d3503aaf9132f65640947cc25ec365f00
ddpiane1 <dominique.pianelli@hobbylobby.com>
2015-10-26 12:04:18 -0500

    News Feed and Dashboard This commit includes the news feed view and dashboard of information about the user logged in. Issue #1

---
+++ TestInstasharp/Common/Configuration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestInstasharp.Common
+{
+    public class Configuration
+    {
+        private static readonly Lazy<Configuration> _instance = new Lazy<Configuration>(() => new Configuration());
+
+        public static Configuration Instance
+        {
+            get
+            {
+                return _instance.Value;
+            }
+        }
+
+        public struct InstagramConfigSection
+        {
+            public string ClientId
+            {
+                get { return "bd42978994b84b69a9797539812d0c55"; }
+            }
+
+            public string ClientSecret
+            {
+                get { return "82add02207a6485bbe6497b410fd6215"; }
+            }
+
+            public string RedirectUri
+            {
+                get { return "http://localhost:5969/Home/OAuth"; }
+            }
+        }
+
+        public InstagramConfigSection InstagramConfig;
+    }
+}
--- TestInstasharp/Controllers/HomeController.cs
+++ TestInstasharp/Controllers/HomeController.cs
@@ -1,15 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Web.Mvc;
+using InstaSharp;
+using InstaSharp.Models.Responses;
 
 namespace TestInstasharp.Controllers
 {
     public class HomeController : Controller
     {
+        readonly InstagramConfig _config = new InstagramConfig(Common.Configuration.Instance.InstagramConfig.ClientId, Common.Configuration.Instance.InstagramConfig.ClientSecret, Common.Configuration.Instance.InstagramConfig.RedirectUri, "");
+
+
+        // GET: Home
+        public ActionResult Dashboard()
+        {
+            var oAuthResponse = Session["InstaSharp.AuthInfo"] as OAuthResponse;
+
+            if (oAuthResponse == null)
+            {
+                return RedirectToAction("Login");
+            }
+
+            return View(oAuthResponse.User);
+        }
+
+        public ActionResult Login()
+        {
+            var scopes = new List<OAuth.Scope>
+            {
+                InstaSharp.OAuth.Scope.Likes,
+                InstaSharp.OAuth.Scope.Comments,
+                InstaSharp.OAuth.Scope.Relationships,
+                InstaSharp.OAuth.Scope.Basic
+            };
+
+            var link = InstaSharp.OAuth.AuthLink(_config.OAuthUri + "authorize", _config.ClientId, _config.RedirectUri, scopes, InstaSharp.OAuth.ResponseType.Code);
+
+            return Redirect(link);
+        }
+
+        public async Task<ActionResult> MyFeed()
+        {
+            var oAuthResponse = Session["InstaSharp.AuthInfo"] as OAuthResponse;
+
+            if (oAuthResponse == null)
+            {
+                return RedirectToAction("Login");
+            }
+
+            var users = new InstaSharp.Endpoints.Users(_config, oAuthResponse);
+
+            var feed = await users.Feed(null, null, null);
+
+            return View(feed.Data);
+        }
+
+        public async Task<ActionResult> OAuth(string code)
+        {
+            // add this code to the auth object
+            var auth = new OAuth(_config);
+
+            // now we have to call back to instagram and include the code they gave us
+            // along with our client secret
+            var oauthResponse = await auth.RequestToken(code);
+
+            // both the client secret and the token are considered sensitive data, so we won't be
+            // sending them back to the browser. we'll only store them temporarily.  If a user's session times
+            // out, they will have to click on the authenticate button again - sorry bout yer luck.
+            Session.Add("InstaSharp.AuthInfo", oauthResponse);
+
+            // all done, lets redirect to the home controller which will send some intial data to the app
+            return RedirectToAction("Dashboard");
+        }
+
         public ActionResult Index()
         {
+            ViewBag.Message = "Home Screen";
             return View();
         }
 
